b0580dfe88cd3fe5027885b197ce5eed4f3c0a5a
dioamorim <diogenesfcamorim@gmail.com>
2024-10-15 00:24:35 +0100

    entrega projeto

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ let xp = 10000;
 let nivel = "";
 
 if (xp <= 1000){
-    nivel = "Ferro";
+    nivel = "Ferro"
 }else if (xp >=1001 && xp <= 2000){
-    nivel = "Bronze";
+    nivel = "Bronze"
 }else if (xp >=2001 && xp <= 5000){
-    nivel = "Prata";
+    nivel = "Prata"
 }else if (xp >=5001 && xp <= 7000){
-    nivel = "Ouro";
+    nivel = "Ouro"
 }else if (xp >=7001 && xp <= 8000){
-    nivel = "Platina";
+    nivel = "Platina"
 }else if (xp >=8001 && xp <= 9000){
-    nivel = "Ascendente";
+    nivel = "Ascendente"
 }else if (xp >=9001 && xp <= 10000){
-    nivel = "Imortal";
+    nivel = "Imortal"
 }else{
-    nivel = "Radiante";
+    nivel = "Radiante"
 }
 
 
